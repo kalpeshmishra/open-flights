f2d69b552fe4f49e76428a977d9b052603e4fae4
Kalpesh Mishra <kalpeshmishra8@gmail.com>
2020-10-29 14:54:59 +0530

    Added my project

---
+++ app/javascript/components/Airline/Airline.js
@@ -0,0 +1,94 @@
+import React, {useState, useEffect, Fragment } from 'react'
+import axios from 'axios'
+import Header from './Header'
+import ReviewForm from './ReviewForm'
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+	margin-left: auto;
+	margin-right: auto;
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+`
+const Column = styled.div`
+	background: #fff;
+	heidht: 100vh;
+	overflow: scroll;
+
+	&:last-child {
+		background: #000;
+	}
+`
+const Main = styled.div`
+	left-padding: 50px;
+`
+
+const Airline = (props) => {
+	const [airline, setAirline] = useState({})
+	const [review, setReview] = useState({})
+	const [loaded, setLoaded] = useState(false)
+
+	useEffect(()=>{
+		const slug = props.match.params.slug
+		const url = `/api/v1/airlines/${slug}`
+
+		axios.get(url)
+		.then( resp => {
+			setAirline(resp.data)
+			setLoaded(true)
+		})
+		.catch( resp => console.log(resp) )
+	},[])
+
+	const handleChange = (e) => {
+		e.preventDefault()
+
+		setReview(Object.assign({}, review, {[e.target.name]: e.target.value}))
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault()	
+		const csrfToken = document.querySelector('[name=csrf-token]').content
+		axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken
+
+		const airline_id = airline.data.id
+		axios.post('api/v1/reviews',{review, airline_id})
+		.then(resp=>{
+			const included = [...airline.included, resp.data]
+			setAirline({...airline, included})
+			setReview({title:'', description:'', score: 0})
+		})
+		.catch(resp=>{
+
+		})
+	}
+
+	return (
+		<Wrapper>
+			{
+				loaded &&
+				<Fragment>
+					<Column>
+						<Main>
+							<Header
+								attributes={airline.data.attributes}
+								reviews={airline.included}
+							/>
+							<div className="reviews"></div>
+						</Main>	
+					</Column>
+					<Column>
+						<ReviewForm
+							handleChange={handleChange}
+							handleSubmit={handleSubmit}
+							attributes={airline.data.attributes}
+							review={review}
+						/>
+					</Column>
+				</Fragment>
+			}	
+		</Wrapper>
+	)
+}
+
+export default Airline
